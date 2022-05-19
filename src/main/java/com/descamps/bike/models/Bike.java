5f45ecad8a98a7df2c6f5a0adc9824957c84c6fb
Kevin Descamps <kevin.descamps2@soprasteria.com>
2022-05-19 16:56:12 +0200

    First commit

---
+++ src/main/java/com/descamps/bike/models/Bike.java
@@ -0,0 +1,15 @@
+package com.descamps.bike.models;
+
+import java.math.BigDecimal;
+
+public class Bike {
+
+    private String name;
+    private String email;
+    private String phone;
+    private String model;
+    private String serialNumber;
+    private BigDecimal purchasePrice;
+    private boolean contact;
+
+}
---
+++ src/test/java/com/descamps/bike/BikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.descamps.bike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
